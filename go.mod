1b27f32cc21c4f85e475795c1033f4ad2d6cba52
Anirudh Oppiliappan <x@icyphox.sh>
2022-12-14 21:14:34 +0530

    routes: multiplex git and web ui

--- go.mod
+++ go.mod
@@ -18,12 +18,14 @@ require (
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/pjbgf/sha1cd v0.2.3 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/skeema/knownhosts v1.1.0 // indirect
+	github.com/sosedoff/gitkit v0.3.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/mod v0.7.0 // indirect
--- routes/handler.go
+++ routes/handler.go
@@ -1,26 +1,65 @@
 package routes
 
 import (
+	"log"
 	"net/http"
+	"path/filepath"
+	"regexp"
 
 	"github.com/alexedwards/flow"
+	"github.com/sosedoff/gitkit"
 	"icyphox.sh/legit/config"
 )
+
+type depsWrapper struct {
+	actualDeps deps
+	gitsvc     *gitkit.Server
+}
+
+// Checks for gitprotocol-http(5) specific query params; if found, passes
+// the request on to the git http service, else render the web frontend.
+func (dw *depsWrapper) Multiplex(w http.ResponseWriter, r *http.Request) {
+	path := flow.Param(r.Context(), "...")
+	name := flow.Param(r.Context(), "name")
+	name = filepath.Clean(name)
+	gitCommand := regexp.MustCompile(`git-(upload|receive)-pack`)
+
+	if path == "info/refs" && gitCommand.MatchString(r.URL.RawQuery) && r.Method == "GET" {
+		dw.gitsvc.ServeHTTP(w, r)
+	} else if gitCommand.MatchString(path) && r.Method == "POST" {
+		dw.gitsvc.ServeHTTP(w, r)
+	} else if r.Method == "GET" {
+		log.Println("index:", r.URL.String())
+		dw.actualDeps.Index(w, r)
+	}
+}
 
 func Handlers(c *config.Config) *flow.Mux {
 	mux := flow.New()
 	d := deps{c}
+
+	gitsvc := gitkit.New(gitkit.Config{
+		Dir:        c.Repo.ScanPath,
+		AutoCreate: false,
+	})
+	if err := gitsvc.Setup(); err != nil {
+		log.Fatalf("git server: %s", err)
+	}
+
+	dw := depsWrapper{actualDeps: d, gitsvc: gitsvc}
 
 	mux.NotFound = http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		d.Write404(w)
 	})
 
 	mux.HandleFunc("/", d.Index, "GET")
-	mux.HandleFunc("/:name", d.RepoIndex, "GET")
+	mux.HandleFunc("/:name", dw.Multiplex, "GET", "POST")
+	mux.HandleFunc("/:name/...", dw.Multiplex, "GET", "POST")
 	mux.HandleFunc("/:name/tree/:ref/...", d.RepoTree, "GET")
 	mux.HandleFunc("/:name/blob/:ref/...", d.FileContent, "GET")
 	mux.HandleFunc("/:name/log/:ref", d.Log, "GET")
 	mux.HandleFunc("/:name/commit/:ref", d.Diff, "GET")
 	mux.HandleFunc("/:name/refs", d.Refs, "GET")
+
 	return mux
 }
