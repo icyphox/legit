08c49d4dd3c60ae9ea1f645b277cdd5cc8f2b0c1
Anirudh Oppiliappan <x@icyphox.sh>
2022-12-24 15:05:04 +0530

    unveil: remove git executable and /dev/null

--- main.go
+++ main.go
@@ -20,12 +20,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := Unveil("/usr/local/bin/git", "rx"); err != nil {
-		log.Fatalf("unveil: %s", err)
-	}
-
 	if err := UnveilPaths([]string{
-		"/dev/null",
 		c.Dirs.Static,
 		c.Repo.ScanPath,
 		c.Dirs.Templates,
