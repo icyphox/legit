b2a5ad41f200f24fe772605f0906c5a9d145fccd
quaintdev <rohanrd@outlook.in>
2023-03-22 12:19:13 +0530

    added ssh url

--- routes/routes.go
+++ routes/routes.go
@@ -151,6 +151,7 @@ func (d *deps) RepoIndex(w http.ResponseWriter, r *http.Request) {
 	data["commits"] = commits
 	data["desc"] = getDescription(path)
 	data["servername"] = d.c.Server.Name
+	data["sshurl"] = "git@" + d.c.Server.Name + ":" + d.c.Repo.ScanPath
 
 	if err := t.ExecuteTemplate(w, "repo", data); err != nil {
 		log.Println(err)
